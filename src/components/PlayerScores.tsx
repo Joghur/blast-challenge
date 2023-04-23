52c1aef926d890cf724ee77ea9759ba993416632
martin <joghur@gmail.com>
2023-04-23 14:17:33 +0200

    Added playerscore visuals

--- src/components/PlayerScores.tsx
+++ src/components/PlayerScores.tsx
@@ -26,20 +26,34 @@ const PlayerScores = ({ killStats }: Props) => {
               <TableCell>Name</TableCell>
               <TableCell align="right">Kills</TableCell>
               <TableCell align="right">Deaths</TableCell>
+              <TableCell align="right">+/-</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {killStats.map((stat: KillStats, index: number) => (
-              <TableRow
-                key={index}
-                sx={{
-                  '&:last-child td, &:last-child th': { border: 0 },
-                }}>
-                <TableCell>{stat.name}</TableCell>
-                <TableCell align="right">{stat.kills}</TableCell>
-                <TableCell align="right">{stat.deads}</TableCell>
-              </TableRow>
-            ))}
+            {killStats.map((stat: KillStats, index: number) => {
+              let scoreDiff;
+              if (stat.kills && stat.deads) {
+                scoreDiff = stat.kills - stat.deads;
+              }
+              return (
+                <TableRow
+                  key={index}
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+                  }}>
+                  <TableCell>{stat.name}</TableCell>
+                  <TableCell align="right">{stat.kills}</TableCell>
+                  <TableCell align="right">{stat.deads}</TableCell>
+                  {scoreDiff && (
+                    <TableCell align="right">
+                      <Typography color={scoreDiff < 0 ? 'red' : 'blue'}>
+                        {scoreDiff}
+                      </Typography>
+                    </TableCell>
+                  )}
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
